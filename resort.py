5e0f20b6742305c0a8184954211d52691a4a40ce
Maria Jose <maria.rivera1@udea.edu.co>
2025-11-24 21:52:49 -0500

    Crear conexion y menu principal

---
+++ resort.py
@@ -0,0 +1,49 @@
+import os
+import csv
+
+def leer_alistamiento():
+    datos = {}
+    with open("alistamiento.txt", "r", encoding="utf-8") as f:
+        for linea in f:
+            k, v = linea.strip().split("=")
+            try:
+                if "." in v:
+                    v = float(v)
+                else:
+                    v = int(v)
+            except:
+                pass
+            datos[k] = v
+    return datos
+
+
+def guardar_alistamiento(datos):
+    with open("alistamiento.txt", "w", encoding="utf-8") as f:
+        for k, v in datos.items():
+            f.write(f"{k}={v}\n")
+
+
+def validar_nombre(n):
+    return n.isalpha() and len(n) >= 3
+
+def validar_doc(d):
+    return d.isdigit() and 3 <= len(d) <= 15
+
+continuar = True
+
+while continuar:
+
+    print("""
+===================================================
+          WAIRA RESORT NATURAL SYSTEM
+====================================================
+1. Planeación de la demanda
+2. Registro de clientes
+3. Registro de clientes registrados
+4. Control de mascotas registradas
+5. Cálculo automático de costos, ventas y ganancias
+6. Exportar administración (Requiere validación)
+7. Salir
+""")
+
+    opcion = input("Seleccione una opción: ")
